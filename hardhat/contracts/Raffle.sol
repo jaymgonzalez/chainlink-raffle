59adb92b1158d88f4ac90e72165c586914fdc97d
Jay M Gonzalez <jaimungon@gmail.com>
2022-11-17 12:28:46 +0100

    add fulfill randomness

--- hardhat/contracts/Raffle.sol
+++ hardhat/contracts/Raffle.sol
@@ -5,20 +5,26 @@ import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 
 error Raffle__NotEnoughEthEntered();
+error Raffle__TransactionNotComplete();
 
 contract Raffle is VRFConsumerBaseV2 {
     // State Variables
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
     uint256 private immutable i_entranceFee;
     address payable[] private s_players;
-    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
     bytes32 private immutable i_gasLane;
     uint64 private immutable i_subscriptionId;
     uint16 private constant REQUEST_CONFIRMATION = 3;
     uint32 private immutable i_callbackGasLimit;
     uint32 private constant NUM_WORDS = 1;
 
+    // Lottery Variables
+    address private s_recentWinner;
+
     // Events
     event RaffleEnter(address indexed player);
+    event RequestedRandomWinner(uint256 indexed requestId);
+    event WinnerPicked(address indexed winner);
 
     constructor(
         address vrfCoordinatorV2,
@@ -44,19 +50,29 @@ contract Raffle is VRFConsumerBaseV2 {
     }
 
     function requestRandomWinner() external {
-        i_vrfCoordinator.requestRandomWords(
+        uint256 requestId = i_vrfCoordinator.requestRandomWords(
             i_gasLane,
             i_subscriptionId,
             REQUEST_CONFIRMATION,
             i_callbackGasLimit,
             NUM_WORDS
         );
+        emit RequestedRandomWinner(requestId);
     }
 
-    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords)
+    function fulfillRandomWords(uint256, uint256[] memory randomWords)
         internal
         override
-    {}
+    {
+        uint256 indexOfWinner = randomWords[0] % s_players.length;
+        address payable recentWinner = s_players[indexOfWinner];
+        s_recentWinner = recentWinner;
+        (bool success, ) = recentWinner.call{value: address(this).balance}("");
+        if (!success) {
+            revert Raffle__TransactionNotComplete();
+        }
+        emit WinnerPicked(recentWinner);
+    }
 
     // view / pure functions
     function getEntranceFee() public view returns (uint256) {
@@ -65,5 +81,9 @@ contract Raffle is VRFConsumerBaseV2 {
 
     function getPlayers(uint256 index) public view returns (address) {
         return s_players[index];
+    }
+
+    function getRecentWinner() public view returns (address) {
+        return s_recentWinner;
     }
 }
