4a79bc5012f8f7793f8a90e8031990f0bcf696ba
Jay M Gonzalez <jaimungon@gmail.com>
2022-11-17 10:37:02 +0100

    add vrf

--- hardhat/contracts/Raffle.sol
+++ hardhat/contracts/Raffle.sol
@@ -1,9 +1,11 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.7;
 
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+
 error Raffle__NotEnoughEthEntered();
 
-contract Raffle {
+contract Raffle is VRFConsumerBaseV2 {
     // State Variables
     uint256 private immutable i_entranceFee;
     address payable[] private s_players;
@@ -11,7 +13,9 @@ contract Raffle {
     // Events
     event RaffleEnter(address indexed player);
 
-    constructor(uint256 entranceFee) {
+    constructor(address vrfCoordinatorV2, uint256 entranceFee)
+        VRFConsumerBaseV2(vrfCoordinatorV2)
+    {
         i_entranceFee = entranceFee;
     }
 
@@ -24,8 +28,14 @@ contract Raffle {
         emit RaffleEnter(msg.sender);
     }
 
-    // function pickRandomWinner() {}
+    function requestRandomWinner() external {}
 
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords)
+        internal
+        override
+    {}
+
+    // view / pure functions
     function getEntranceFee() public view returns (uint256) {
         return i_entranceFee;
     }
