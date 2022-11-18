d25c91e06079f8830617abc85ea50096d6c8a98e
Jay M Gonzalez <jaimungon@gmail.com>
2022-11-18 13:21:05 +0100

    change testnet to goerli

--- hardhat/helper-hardhat-config.ts
+++ hardhat/helper-hardhat-config.ts
@@ -17,22 +17,22 @@ export interface networkConfigInfo {
 export const networkConfig: networkConfigInfo = {
   31337: {
     name: 'localhost',
-    subscriptionId: '588',
+    subscriptionId: '6615',
     gasLane:
       '0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc', // 30 gwei
     keepersUpdateInterval: '30',
     raffleEntranceFee: ethers.utils.parseEther('0.01').toString(), // 0.01 ETH
     callbackGasLimit: '500000', // 500,000 gas
   },
-  4: {
-    name: 'rinkeby',
-    subscriptionId: '588',
+  5: {
+    name: 'goerli',
+    subscriptionId: '6615',
     gasLane:
       '0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc', // 30 gwei
     keepersUpdateInterval: '30',
     raffleEntranceFee: ethers.utils.parseEther('0.01').toString(), // 0.01 ETH
     callbackGasLimit: '500000', // 500,000 gas
-    vrfCoordinatorV2: '0x6168499c0cFfCaCD319c818142124B7A15E857ab',
+    vrfCoordinatorV2: '0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D',
   },
   1: {
     name: 'mainnet',
