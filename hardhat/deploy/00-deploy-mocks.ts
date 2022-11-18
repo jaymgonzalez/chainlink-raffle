90a98b23cbd4a13b961f54d73deabf0b2963d9be
Jay M Gonzalez <jaimungon@gmail.com>
2022-11-18 10:04:32 +0100

    finish deploy script

--- hardhat/deploy/00-deploy-mocks.ts
+++ hardhat/deploy/00-deploy-mocks.ts
@@ -1,7 +1,8 @@
+import { ethers } from 'hardhat'
 import { DeployFunction } from 'hardhat-deploy/types'
 import { HardhatRuntimeEnvironment } from 'hardhat/types'
 
-const BASE_FEE = '250000000000000000' // 0.25 is this the premium in LINK?
+const BASE_FEE = ethers.utils.parseEther('0.25').toString() // 0.25 is this the premium in LINK?
 const GAS_PRICE_LINK = 1e9 // link per gas, is this the gas lane? // 0.000000001 LINK per gas
 
 const deployMocks: DeployFunction = async function (
--- hardhat/deploy/01-deploy-raffle.ts
+++ hardhat/deploy/01-deploy-raffle.ts
@@ -1,3 +1,4 @@
+import { ethers } from 'hardhat'
 import { HardhatRuntimeEnvironment } from 'hardhat/types'
 import { DeployFunction } from 'hardhat-deploy/types'
 import {
@@ -5,7 +6,9 @@ import {
   developmentChains,
   VERIFICATION_BLOCK_CONFIRMATIONS,
 } from '../helper-hardhat-config'
-// import verify from '../utils/verify'
+import verify from '../utils/verify'
+
+const FUND_AMOUNT = ethers.utils.parseEther('30').toString()
 
 const deployRaffle: DeployFunction = async function (
   hre: HardhatRuntimeEnvironment
@@ -15,10 +18,40 @@ const deployRaffle: DeployFunction = async function (
   const { deploy, log, get } = deployments
   const { deployer } = await getNamedAccounts()
   const chainId = 31337
+  let vrfCoordinatorV2Address: string | undefined,
+    subscriptionId: string | undefined
+
+  if (chainId == 31337) {
+    // create VRFV2 Subscription
+    const vrfCoordinatorV2Mock = await ethers.getContract(
+      'VRFCoordinatorV2Mock'
+    )
+    vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address
+    const transactionResponse = await vrfCoordinatorV2Mock.createSubscription()
+    const transactionReceipt = await transactionResponse.wait()
+    subscriptionId = transactionReceipt.events[0].args.subId
+    // Fund the subscription
+    // Our mock makes it so we don't actually have to worry about sending fund
+    await vrfCoordinatorV2Mock.fundSubscription(subscriptionId, FUND_AMOUNT)
+  } else {
+    vrfCoordinatorV2Address =
+      networkConfig[network.config.chainId!]['vrfCoordinatorV2']
+    subscriptionId = networkConfig[network.config.chainId!]['subscriptionId']
+  }
 
   const waitBlockConfirmations = developmentChains.includes(network.name)
     ? 1
     : VERIFICATION_BLOCK_CONFIRMATIONS
+
+  log('----------------------------------------------------')
+  const args: any[] = [
+    vrfCoordinatorV2Address,
+    subscriptionId,
+    networkConfig[network.config.chainId!]['gasLane'],
+    networkConfig[network.config.chainId!]['keepersUpdateInterval'],
+    networkConfig[network.config.chainId!]['raffleEntranceFee'],
+    networkConfig[network.config.chainId!]['callbackGasLimit'],
+  ]
 
   const raffle = await deploy('Raffle', {
     from: deployer,
@@ -26,6 +59,19 @@ const deployRaffle: DeployFunction = async function (
     log: true,
     waitConfirmations: waitBlockConfirmations,
   })
+
+  // Verify the deployment
+  if (
+    !developmentChains.includes(network.name) &&
+    process.env.ETHERSCAN_API_KEY
+  ) {
+    log('Verifying...')
+    await verify(raffle.address, args)
+  }
+  log('Run Price Feed contract with command:')
+  const networkName = network.name == 'hardhat' ? 'localhost' : network.name
+  log(`yarn hardhat run scripts/enterRaffle.js --network ${networkName}`)
+  log('----------------------------------------------------')
 }
 
 export default deployRaffle
